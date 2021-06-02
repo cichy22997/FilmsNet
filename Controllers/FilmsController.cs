b2db1ba93e546c2e3847061948a2b169a52d5613
Wojciech Cichaczewski <wojciech.cichaczewski@gmail.com>
2021-06-02 02:23:53 +0200

    Add https requests

--- Controllers/FilmsController.cs
+++ Controllers/FilmsController.cs
@@ -1,16 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FilmsNet.Data;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
+using FilmsNet.Models;
 
 namespace FilmsNet.Controllers
 {
     public class FilmsController : Controller
     {
+        private readonly ApplicationDbContext ctx;
+        public FilmsController(ApplicationDbContext context)
+        {
+            ctx = context;
+        }
+        [HttpGet]
+        [Route("/api/GetFilms")]
         public IActionResult Index()
         {
-            return View();
+            var filmList = ctx.Films.ToList();
+            var json = JsonSerializer.Serialize(filmList);
+            return Ok(json);
         }
+
+        [HttpPost("/api/AddFilm/{title}/{year}")]
+        public async Task<IActionResult> AddFilm(string title, int year)
+        {
+            Film film = new Film()
+            {
+                Id = 2,
+                Title = title,
+                Year = year
+            };
+            var res = ctx.Films.Add(film);
+
+            ctx.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPatch("/api/PatchFilm/{id}/{title}/{year}")]
+        public async Task<IActionResult> UpdateFilm(int id, string title, int year)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("/api/DeleteFilm/{id}")]
+        public async Task<IActionResult> DeleteFilm(int id)
+        {
+            return Ok();
+        }
+
     }
 }
--- clientapp/src/App.vue
+++ clientapp/src/App.vue
@@ -1,153 +1,199 @@
 <template>
-	<div class="container">
-		<Header />
-		<Films
-			@add-film="onAddFilm"
-			@update-film="onUpdateFilm"
-			@delete-film="onDeleteFilm"
-			:films="films"
-		/>
-		<Modal
-			:modalHeader="modalHeader"
-			:modalYear="modalYear"
-			:modalTitle="modalTitle"
-			:modalId="modalId"
-			v-show="isModalVisible"
-			@close="closeModal"
-			@submit="submitModal"
-		/>
-		<Footer />
-	</div>
+  <div class="container">
+    <Header />
+    <Films
+      @add-film="onAddFilm"
+      @update-film="onUpdateFilm"
+      @delete-film="onDeleteFilm"
+      :films="films"
+    />
+    <Modal
+      :modalHeader="modalHeader"
+      :modalYear="modalYear"
+      :modalTitle="modalTitle"
+      :modalId="modalId"
+      v-show="isModalVisible"
+      @close="closeModal"
+      @submit="submitModal"
+    />
+    <Footer />
+  </div>
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import Films from './components/Films.vue'
-import axios from 'axios' 
+import Modal from "./components/Modal.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Films from "./components/Films.vue";
+import axios from "axios";
 
 export default {
-	name: 'App',
-	components: {
-		Header,
-		Footer,
-		Films,
-		Modal
-	},
-	data() {
-		return {
-			films: [],
-			isModalVisible: false,
-			modalHeader: '',
-			modalId: null,
-			modalTitle: null,
-			modalYear: null,
-			isModalAdding: false,
-			isModalUpdating: false
-		}
-	},
-	methods: {
-		async onDeleteFilm(id, index) {
-			if (
-				confirm('Do you want to delete '+this.films[index].title+'?')
-			)
-				return
-		},
-		onUpdateFilm(id, index) {
-			this.isModalUpdating = true
-			this.isModalVisible = true
-			var exactFilm = this.films[index]
-			this.modalId = id
-			this.modalTitle = exactFilm.title
-			this.modalYear = exactFilm.year
-			this.modalHeader = 'Update Film'
-		},
-		onAddFilm() {
-			this.isModalAdding = true
-			this.isModalVisible = true
-			this.modalTitle = ''
-			this.modalYear = null
-			this.modalHeader = 'Add new Film'
-		},
-		closeModal() {
-			this.isModalVisible = false
-		},
-		submitModal() {
-			if (this.isModalAdding) {
-				this.isModalAdding = false
-			} else if (this.isModalUpdating) {
-				this.isModalUpdating = false
-			}
-		}
-	},
-	mounted: function(){
-
-	},
-	created() {
-		this.films = [
-			{
-				id: 1,
-				title: 'Assasins Creed',
-				year: 2019
-			},
-			{
-				id: 2,
-				title: 'People',
-				year: 1997
-			},
-			{
-				id: 11,
-				title: 'Boom',
-				year: 2006
-			},
-			{
-				id: 4,
-				title: 'Assasins Creed',
-				year: 2019
-			},
-			{
-				id: 5,
-				title: 'People',
-				year: 1997
-			},
-			{
-				id: 6,
-				title: 'Boom',
-				year: 2006
-			},
-			{
-				id: 7,
-				title: 'People',
-				year: 1997
-			},
-			{
-				id: 8,
-				title: 'Boom',
-				year: 2006
-			},
-			{
-				id: 9,
-				title: 'Assasins Creed',
-				year: 2019
-			}
-		]
-	}
-}
+  name: "App",
+  components: {
+    Header,
+    Footer,
+    Films,
+    Modal,
+  },
+  data() {
+    return {
+      films: [],
+      isModalVisible: false,
+      modalHeader: "",
+      modalId: null,
+      modalTitle: null,
+      modalYear: null,
+      isModalAdding: false,
+      isModalUpdating: false,
+      loading: true,
+    };
+  },
+  methods: {
+    async onDeleteFilm(id, index) {
+      if (confirm("Do you want to delete " + this.films[index].title + "?")) {
+        await this.deleteFilm(id);
+        await this.getFilmsList();
+      } else return;
+    },
+    onUpdateFilm(id, index) {
+      this.isModalUpdating = true;
+      this.isModalVisible = true;
+      var exactFilm = this.films[index];
+      this.modalId = id;
+      this.modalTitle = exactFilm.title;
+      this.modalYear = exactFilm.year;
+      this.modalHeader = "Update Film";
+    },
+    onAddFilm() {
+      this.isModalAdding = true;
+      this.isModalVisible = true;
+      this.modalTitle = "";
+      this.modalYear = null;
+      this.modalHeader = "Add new Film";
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
+    async submitModal(id, title, year) {
+      if (this.isModalAdding) {
+        await this.addFilm(title, year);
+        await this.getFilmsList();
+        this.isModalAdding = false;
+      } else if (this.isModalUpdating) {
+        await this.updateFilm(id, title, year);
+        await this.getFilmsList();
+        this.isModalUpdating = false;
+      }
+      this.closeModal();
+    },
+    async getFilmsList() {
+      try {
+        const response = await axios.get("/api/GetFilms");
+        alert(response);
+      } catch (error) {
+        alert(error+"error");
+      }
+    },
+    async addFilm(title, year) {
+        await axios
+          .post("/api/AddFilm/"+title+"/"+year)
+          .then(function (response) {
+            alert("OK"+response);
+          })
+          .catch(function (error) {
+            alert(error);
+          });
+    },
+    async updateFilm(id, title, year) {
+        await axios
+          .patch("/api/PatchFilm/"+id+"/"+title+"/"+year)
+          .then(function (response) {
+            alert("OK"+response);
+          })
+          .catch(function (error) {
+            alert(error);
+          });
+    },
+    async deleteFilm(id) {
+      const path = "/api/DeleteFilm/" + id;
+      await axios
+        .delete(path)
+        .then(function (response) {
+          alert("OK"+response);
+        })
+        .catch(function (error) {
+          alert(error);
+        });
+    },
+  },
+  mounted() {
+    this.getFilmsList();
+  },
+  created() {
+    this.films = [
+      {
+        id: 1,
+        title: "Assasins Creed",
+        year: 2019,
+      },
+      {
+        id: 2,
+        title: "People",
+        year: 1997,
+      },
+      {
+        id: 11,
+        title: "Boom",
+        year: 2006,
+      },
+      {
+        id: 4,
+        title: "Assasins Creed",
+        year: 2019,
+      },
+      {
+        id: 5,
+        title: "People",
+        year: 1997,
+      },
+      {
+        id: 6,
+        title: "Boom",
+        year: 2006,
+      },
+      {
+        id: 7,
+        title: "People",
+        year: 1997,
+      },
+      {
+        id: 8,
+        title: "Boom",
+        year: 2006,
+      },
+      {
+        id: 9,
+        title: "Assasins Creed",
+        year: 2019,
+      },
+    ];
+  },
+};
 </script>
 
 <style>
 body {
-	background-color: #dbdbdb;
-	font-family: 'Lato', sans-serif;
+  background-color: #dbdbdb;
+  font-family: "Lato", sans-serif;
+  margin: 0;
 }
 
 .btn {
-	width: 80px;
-	height: 30px;
-	border: 0;
-	color: #fff;
-	border-radius: 5px;
+  width: 80px;
+  height: 30px;
+  border: 0;
+  color: #fff;
+  border-radius: 5px;
 }
 
 .container {
